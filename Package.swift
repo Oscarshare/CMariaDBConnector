420f117905f5a7100d27115baccc301238536b9f
Xu Lian <xw901103@gmail.com>
2017-11-12 12:27:46 +1100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CMariaDBConnector",
+    products: [
+        .library(name: "CMariaDBConnector", targets: ["CMariaDBConnector"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(name: "CMariaDBConnector", dependencies: [])
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# CMariaDBConnector
+
+A description of this package.
